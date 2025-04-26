45070a1e8ae01ecac238705cd197cddb663e19ef
thelastgoth128 <thindwacyrus86@gmail.com>
2025-04-26 16:31:02 +0200

    put the draw function in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "GLFW/glfw3.h"
 #include <iostream>
 #include "resources/shaders/shader.cpp"
-#include "resources/Sphere/Sphere.cpp"
+#include "Planet.cpp"
 
 using namespace std;
 
@@ -28,51 +28,16 @@ int main () {
 
     glViewport(0, 0, 800, 600);
 
-    Sphere sphere;
-
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    // Vertex data (interleaved V/N/T)
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER,sphere.getInterleavedVertexSize(), sphere.getInterleavedVertices(), GL_STATIC_DRAW);
-
-    // Index data
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sphere.getIndexSize(), sphere.getIndices(), GL_STATIC_DRAW);
-
-    // Vertex Attributes
-    GLsizei stride = sphere.getInterleavedStride();
-
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // Normal attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // Texture coord attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    glBindVertexArray(0);
-
 
     Shader ourShader("C:\\Users\\thind\\Documents\\projects\\Solar-System\\src\\resources\\shaders\\vertex.vs","C:\\Users\\thind\\Documents\\projects\\Solar-System\\src\\resources\\shaders\\fragment.fss");
+    Planet Sun("Sun","C:\\Users\\thind\\Documents\\projects\\Solar-System\\src\\resources\\PlanetTextureMaps\\sunmap.jpg",1.0f,0.0f,0.2f,0.2f,nullptr,ourShader.ID);
 
     while(!glfwWindowShouldClose(window)) {
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES,sphere.getIndexCount(), GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+        Sun.Draw();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
