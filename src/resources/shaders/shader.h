25b2f514044ddbe866105c96a2cff6916399c7fc
thelastgoth128 <thindwacyrus86@gmail.com>
2025-04-26 10:52:57 +0200

    project initialization

---
+++ src/resources/shaders/shader.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "glad/glad.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+class Shader {
+    public:
+    //the program ID
+    unsigned int ID;
+
+    //constructor reads and builds the shader
+    Shader(const char* vertexPath, const char* fragmentPath);
+    //use/activate the shader
+    void use();
+    //utility uniform functions
+    void setBool(const string &name, bool value) const;
+    void setInt(const string &name, int value) const;
+    void setFloat(const string &name, float value) const;
+};
